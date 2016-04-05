5416ce2621bcdf2ded99d4f7702131a35360b779
Bondar Pavel <bond.p85@gmail.com>
2016-04-05 16:04:56 +0300

    HW1 -> checking how return works

--- HW1/HW1/main.c
+++ HW1/HW1/main.c
@@ -27,17 +27,24 @@ void ICPrintHelloWorld () {
 
 
 int main(int argc, const char * argv[]) {
-  return 0;
+    // return 0;
     printf("Hello, World!\n");
+    // return 0;
     printf("Nice to meet you\n");
+    // return 0;
     printf("My name is: ");
+    // return 0;
     printf("Pasha\n");
+    // return 0;
     
     ICPrintHelloWorld();      //Calls function ICPrintHelloW
+    // return 0;
     ICPrintHelloWorld();      // can call function as much as we need
+    // return 0;
     ICPrintHelloWorld();
+    // return 0;
     ICPrintHelloWorld();
-    
+    return 0;            // return stops function impementation and go out from the function with return value with type of function or nothing if void function
     
     
 //    ICPrintHelloWorld();    //Code after "return" will not run. "Return" ends the function
