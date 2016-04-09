bac5cf347a26b151e155b1cc642763f5afd18d26
Bondar Pavel <bond.p85@gmail.com>
2016-04-09 21:47:14 +0300

    HWDataTypes1 -> Refactored previous code. BPVRunApplication added with BPVLauncher function

---
+++ HWDataTypes1/Sources/BPVRunApplication.c
@@ -0,0 +1,9 @@
+//
+//  BPVRunApplication.c
+//  HWDataTypes1
+//
+//  Created by Bondar Pavel on 4/9/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#include "BPVRunApplication.h"
---
+++ HWDataTypes1/Sources/BPVRunApplication.h
@@ -0,0 +1,14 @@
+//
+//  BPVRunApplication.h
+//  HWDataTypes1
+//
+//  Created by Bondar Pavel on 4/9/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#ifndef BPVRunApplication_h
+#define BPVRunApplication_h
+
+#include <stdio.h>
+
+#endif /* BPVRunApplication_h */
