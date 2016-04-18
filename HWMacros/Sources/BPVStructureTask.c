c901d59872394376fd81a36049108305810a4b03
Bondar Pavel <bond.p85@gmail.com>
2016-04-18 19:15:05 +0300

    HWMacro -> Started structure task

---
+++ HWMacros/Sources/BPVStructureTask.c
@@ -0,0 +1,10 @@
+//
+//  BPVStructureTask.c
+//  HWMacros
+//
+//  Created by Bondar Pavel on 4/18/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#include "BPVStructureTask.h"
+
---
+++ HWMacros/Sources/BPVStructureTask.h
@@ -0,0 +1,37 @@
+//
+//  BPVStructureTask.h
+//  HWMacros
+//
+//  Created by Bondar Pavel on 4/18/16.
+//  Copyright © 2016 Pavel Bondar. All rights reserved.
+//
+
+#ifndef BPVStructureTask_h
+#define BPVStructureTask_h
+
+#include <stdbool.h>
+
+#include <stddef.h>
+
+typedef struct {
+    bool boolValue1;
+    int intValue;
+    float floatValue;
+    bool boolValue2;
+    short shortValue1;
+    bool boolValue3;
+    const char *string;
+    long long longLongValue;
+    bool boolValue4;
+    short shortValue2;
+    bool boolVAlue5;
+    double doubleValue;
+    short shortValue3;
+} BPVTypesStructure;
+
+#pragma mark -
+#pragma mark Functions for structures
+
+
+
+#endif /* BPVStructureTask_h */
